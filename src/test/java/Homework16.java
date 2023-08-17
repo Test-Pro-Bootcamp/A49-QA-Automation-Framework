12d3bcb7bdcb19e248d49698c2abf40d74f9e038
rijoal <114624987+rijoal@users.noreply.github.com>
2023-08-16 18:56:49 -0700

    Create Homework16.java

---
+++ src/test/java/Homework16.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Homework16 extends BaseTest{
+
+    @Test
+    public static void Navigation() {
+
+        WebDriver driver = new ChromeDriver();
+        driver.
+    }
+
+
+}
