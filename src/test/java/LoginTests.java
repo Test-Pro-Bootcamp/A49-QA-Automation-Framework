fb7f2e31064cd5d70e37b4df2da1e2ed03c647ae
rijoal <114624987+rijoal@users.noreply.github.com>
2023-08-16 19:54:43 -0700

    Homework16

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
@@ -9,7 +11,7 @@ import java.time.Duration;
 public class LoginTests extends BaseTest {
 
     @Test
-    public void LoginEmptyEmailPassword() {
+    public void ValidLogin() {
 
 //      Added ChromeOptions argument below to fix websocket error
         ChromeOptions options = new ChromeOptions();
@@ -20,7 +22,80 @@ public class LoginTests extends BaseTest {
 
         String url = "https://qa.koel.app/";
         driver.get(url);
+
+        WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
+        emailField.clear();
+        emailField.sendKeys("dirzo@gmail.com");
+
+        WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
+        passwordField.clear();
+        passwordField.sendKeys("Te$ter1234");
+
+        WebElement submit = driver.findElement(By.cssSelector("button[type='submit']"));
+        submit.click();
+
+        WebElement avatarIcon = driver.findElement(By.cssSelector("img[class='avatar']"));
+
+
+        Assert.assertTrue(avatarIcon.isDisplayed());
+        driver.quit();
+    }
+
+
+        @Test
+        public void loginInvalidEmailValidPassword () {
+
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--remote-allow-origins=*");
+
+            WebDriver driver = new ChromeDriver(options);
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+            String url = "https://qa.koel.app/";
+            driver.get(url);
+
+            WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
+            emailField.clear();
+            emailField.sendKeys("invalid@gmail.com");
+
+            WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
+            passwordField.clear();
+            passwordField.sendKeys("Te$ter1234");
+
+            WebElement submit = driver.findElement(By.cssSelector("button[type='submit']"));
+            submit.click();
+
+            Assert.assertEquals(driver.getCurrentUrl(), url);
+
+            driver.quit();
+        }
+
+
+    @Test
+    public void loginValidEmailInvalidPassword () {
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        String url = "https://qa.koel.app/";
+        driver.get(url);
+
+        WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
+        emailField.clear();
+        emailField.sendKeys("dirzo@gmail.com");
+
+        WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
+        passwordField.clear();
+        passwordField.sendKeys("Te$ter");
+
+        WebElement submit = driver.findElement(By.cssSelector("button[type='submit']"));
+        submit.click();
+
         Assert.assertEquals(driver.getCurrentUrl(), url);
+
         driver.quit();
     }
 }
