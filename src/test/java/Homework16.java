d56379e8a10143ae4d219eec3f2a4f9c4fc49423
NataliaGorbunova1 <142173024+NataliaGorbunova1@users.noreply.github.com>
2023-08-18 17:00:23 -0400

    Please help. WebElement, By, click - "cannot resolve". Test failed - "compileTestJava".

---
+++ src/test/java/Homework16.java
@@ -0,0 +1,35 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Homework16 extends BaseTest {
+
+@Test
+
+public void registrationNavigation() { {
+
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--remote-allow-origins=*");
+    options.addArguments("--start-maximized");
+
+    WebDriver driver = new ChromeDriver(options);
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+    String url = "https://testpro.io/";
+    driver.get(url);
+
+    WebElement registrationLink = driver.findElement(By.cssSelector("//a[@href='registration']"));
+    registrationLink.click();
+
+    Assert.assertEquals(driver.getCurrentUrl(), url);
+    driver.quit();
+
+
+
+}
+}
+}
