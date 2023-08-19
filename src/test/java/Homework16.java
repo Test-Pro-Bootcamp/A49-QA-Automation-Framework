664edfcb86c9e84464888624b490d131f81cb8e6
rijoal <114624987+rijoal@users.noreply.github.com>
2023-08-18 18:10:31 -0700

    Homework 16

--- src/test/java/Homework16.java
+++ src/test/java/Homework16.java
@@ -8,7 +8,7 @@ import org.testng.annotations.Test;
 
 import java.time.Duration;
 
-public class Homework16 extends BaseTest{
+public class Homework16 {
 
     @Test
     public static void registrationNavigation() {
