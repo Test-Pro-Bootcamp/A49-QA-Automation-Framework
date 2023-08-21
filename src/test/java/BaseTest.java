2fd9e05957426d41f2607e4f7650ff8ddfaa295a
MavisAffum <133819225+MavisAffum@users.noreply.github.com>
2023-08-21 13:49:45 -0500

    Song Added to Playlist
    First song selected from Homework17 playlist in Koel

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,5 +1,7 @@
+import com.sun.source.doctree.SeeTree;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
@@ -10,9 +12,10 @@ import org.openqa.selenium.By
 import java.time.Duration;
 
 public class BaseTest {
- public webDriver = null;
+ public WebDriver = "null";
  public String url = "https://qa.koel.app";
- driver.get(url);
+    public WebElement songsToPlaylists;
+  driver.get(url);
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
@@ -20,12 +23,58 @@ public class BaseTest {
 
 
     @BeforeMethod
-    public void addSongsToPlaylists(){
-        chromeOptions options = new chromeOptions();
-        optiens.addArguments("---remote-allow-origins");
-    webDriver driver = new chromeDriver();
-    driver.manage().timeouts.implicitlywaiit(Duration.ofSeconds(10));
+    public void launchBrowser() {
+        public void addSongsToPlaylists () {
+            chromeOptions options = new chromeOptions();
+            optiens.addArguments("---remote-allow-origins");
 
+            webDriver driver = new chromeDriver(options);
+            driver.manage().timeouts.implicitlywaiit(Duration.ofSeconds(10));
+
+
+            public void navigateToPage(){
+
+            public void provideEmail(){
+                email.clear();
+                email.sendkeys(... keysToSend:demo@class.com);
+
+                public void providePassword(){
+                    password.clear();
+                    password.sendkeys(...keysToSend:te$t$tudent);
+
+                    public void clickSubmit(){
+                        Submit.click();
+
+                        public void searchField(String name); throw interruptedExpection {
+              webElement searchField = driver.findElement(By.cssSelector("div[id= 'searchForm']"));
+                searchfield.click();
+                searchfield.sendkeys(...keysToSend);
+                thread.sleep(millis:2000);
+
+            public void clickToViewAllBtn(); throw  interruptedExpection {
+                webElement clickToViewAllBtn = driver.findElement(By.xpath("button[data-test = 'view-all-songs-btn']"));
+                ToViewAllBtn.click();
+                thread.sleep(millis:2000);
+
+
+            public void selectFirstSongResult() throw interruptedExpection{
+            webElement FirstSong = driver.findElement(By.cssSelector("section[@id = 'songResultsWrapper']//tr[@class = 'song-item'][1]"));
+                firstSong.click;
+                thread.sleep(millis:2000);
+
+                public void clickAddToBtn() throw interruptedException{
+                    addToBtn.click();
+                    thread.sleep(millis:2000);
+
+                    public void choosePlaylist throw interruptedExpection(){
+                     webElement  playlist = driver.findElement(By"[]");
+                     playlist.click();
+                     thread.sleep(millis:2000);
+                                }
+                            }
+
+                Assert.assertTrue(driver.getNotificationText().contains(Homework17Created));
+        }
     @AfterMethod
      public void closeBrowser(){
     driver.quit();
--- src/test/java/Homework17.java
+++ src/test/java/Homework17.java
@@ -1,6 +1,23 @@
-public class Homework17 extends BaseTest {
-  public void addSongsToPlaylists(){
+import java.nio.channels.SeekableByteChannel;
 
+public class Homework17 extends BaseTest {
+  public void addSongsToPlaylists() throws interruptedExpection{
+      String ExpectedMessageForAddedSong = Added_1_song_into "Homework17\Koel\Playlist"
+
+      navigateToPage();
+      provideEmail("demo@class.com");
+ providePassword("te$t$tudent");
+ clickSubmit();
+ searchfield("It's Your Birthday");
+ clickToViewAllBtn();
+ selectFirstSong();
+ clickAddToBtn();
+ choosePlaylist();
+ thread.sleep(millis:2000);
+ driver.quit();
 
   }
-}
+
+
+    }
+
