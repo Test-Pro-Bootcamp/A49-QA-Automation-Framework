810ee0e74a1f102d6cfc6a822855f0068f037f5d
BhaktiDamle <bhakti.damle@testpro.io>
2023-08-21 09:26:34 -0700

    Update to homework-17

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
-
 import java.time.Duration;
 
 
@@ -16,6 +15,7 @@ public class BaseTest {
     public static WebDriver driver = null;
 
     @BeforeSuite
+
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
     }
@@ -23,7 +23,6 @@ public class BaseTest {
     @BeforeMethod
 
     public static void launchBrowser() {
-        //      Added ChromeOptions argument below to fix websocket error
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         options.addArguments("--disable-popup-blocking");
@@ -32,56 +31,48 @@ public class BaseTest {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
-    @AfterMethod
-
-    public static void closeBrowser() {
-        driver.quit();
-    }
-
+// Helper Methods
     public static void navigateToPage() {
         String url = "https://qa.koel.app/";
         driver.get(url);
 
     }
-
-    public static void provideEmail(String email){
+    public static void provideEmail(String email) {
         WebElement emailField = driver.findElement(By.xpath("//input[@type='email']"));
         emailField.clear();
         emailField.sendKeys(email);
     }
-
-    public static void providePassword(String password){
+    public static void providePassword(String password) {
         WebElement passwordField = driver.findElement(By.xpath("//input[@type='password']"));
         passwordField.clear();
         passwordField.sendKeys(password);
     }
-
     public static void clickSubmit() throws InterruptedException {
         WebElement submitButton = driver.findElement(By.xpath("//button[@type='submit']"));
         submitButton.click();
         Thread.sleep(2000);
 
     }
-
-    public void searchSong(String song) throws InterruptedException{
+    public void searchSong(String song) throws InterruptedException {
         WebElement searchField = driver.findElement(By.xpath("//input[@type='search']"));
         searchField.sendKeys(song);
         searchField.click();
         Thread.sleep(2000);
     }
+
     public void clickViewAllButton() throws InterruptedException {
-        WebElement viewAllWebelement = driver.findElement(By.cssSelector("div.results section.songs h1 button"));
-        viewAllWebelement .click();
+        WebElement viewAllWebelement = driver.findElement(By.cssSelector("[data-test='view-all-songs-btn']"));
+        viewAllWebelement.click();
         Thread.sleep(2000);
     }
 
-    public static void selectFirstSong()throws InterruptedException{
+    public static void selectFirstSong() throws InterruptedException {
         WebElement firstSong = driver.findElement(By.cssSelector("section#songResultsWrapper tr.song-item td.title"));
         firstSong.click();
         Thread.sleep(2000);
     }
 
-    public void clickAddToButton() throws InterruptedException{
+    public void clickAddToButton() throws InterruptedException {
 
         WebElement addToButton = driver.findElement(By.xpath("//button[@data-test='add-to-btn']"));
 
@@ -89,7 +80,7 @@ public class BaseTest {
         Thread.sleep(2000);
     }
 
-    public void choosePlaylist() throws InterruptedException{
+    public void choosePlaylist() throws InterruptedException {
 
         WebElement addToPlaylist = driver.findElement(By.xpath("*//section[@id='songResultsWrapper']//li[contains(text(),'Old Songs')]"));
         addToPlaylist.click();
@@ -97,15 +88,19 @@ public class BaseTest {
 
     }
 
-    public String getNotification(){
+    public String getNotification() {
 
         WebElement notificationElement = driver.findElement(By.cssSelector("div.success.show"));
         return notificationElement.getText();
 
     }
 
+    @AfterMethod
 
+    public static void closeBrowser() {
 
+        driver.quit();
+    }
 
 
 }
--- src/test/java/Homework17.java
+++ src/test/java/Homework17.java
@@ -6,21 +6,21 @@ import org.testng.annotations.Test;
 import java.time.Duration;
 
 public class Homework17 extends BaseTest {
-@Test
-    public void addSongToPlaylist() throws InterruptedException{
+    @Test
+    public void addSongToPlaylist() throws InterruptedException {
 
-String newSongAddedNotification = "Added 1 song into";
+        String newSongAddedNotification = "Added 1 song into";
 
-    navigateToPage();
-    provideEmail("bhakti.damle@testpro.io");
-    providePassword("Mangodesk234!");
-    clickSubmit();
-    searchSong("Pluto");
-    clickViewAllButton();
-    selectFirstSong();
-    clickAddToButton();
-    choosePlaylist();
-    Assert.assertTrue(getNotification().contains(newSongAddedNotification));
+        navigateToPage();
+        provideEmail("bhakti.damle@testpro.io");
+        providePassword("Mangodesk234!");
+        clickSubmit();
+        searchSong("Saxophonian March");
+        clickViewAllButton();
+        selectFirstSong();
+        clickAddToButton();
+        choosePlaylist();
+        Assert.assertTrue(getNotification().contains(newSongAddedNotification));
 
 
     }
