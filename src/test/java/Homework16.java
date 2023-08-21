b653d6ac7a03ed004796dbe812c32a8ccc74bc8b
LiudmilaChernyh <liudmila.chernyh@testpro.io>
2023-08-21 14:03:59 -0700

    Homework16
    second attempt

--- src/test/java/Homework16.java
+++ src/test/java/Homework16.java
@@ -1,26 +1,39 @@
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+import org. testng.Assert;
 
-public class Homework16 extends BaseTest {
+
+import java.time.Duration;
+
+
+public class Homework16  extends BaseTest{
     @Test
-    public void registrationNavigation(){
-        //Added ChromeOptions argument below to fix websocket error
+    public void registrationNavigation () {
         ChromeOptions options = new ChromeOptions();
-        options.addArguments ("--remote-allow-origins=*");
+        options.addArguments("--remote-allow-origins=*");
+
         WebDriver driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait (Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         String url = "https://qa.koel.app/";
-        driver. get (url);
-        WebElement registrationlink = driver.findElement(By.cssSelector("[href='registration']"));
-        registrationlink.click();
-        String registrationUrl="https://qa.koel.app/registration";
+        driver.get(url);
+        WebElement registrationLink =driver.findElement( By.cssSelector("[type='registration']"));
+        registrationLink.click();
+        String registrationUrl= "https://qa.koel.app/registration";
+
         Assert.assertEquals(driver.getCurrentUrl(),registrationUrl);
         driver.quit();
 
 
 
 
+    }
+
 
     }
-}
+
+
+
