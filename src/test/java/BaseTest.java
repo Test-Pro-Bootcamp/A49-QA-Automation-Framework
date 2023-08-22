e0c5a1606c1ed537f65c43da246f14f06f959b6b
CamdenBertrand <142252732+CamdenBertrand@users.noreply.github.com>
2023-08-22 13:51:11 -0500

    Update BaseTest.java

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -24,11 +24,13 @@ public class BaseTest {
     }
     @BeforeMethod
     public void launchBrowser() {
-        //      Added ChromeOptions argument below to fix websocket error
+
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
+
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
     }
     @AfterMethod
     public void closeBrowser() {
