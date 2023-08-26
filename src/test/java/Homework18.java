1e9a922a498158b1bbef6e516ff6ad07a341725c
suemoua <sue.moua@testpro.io>
2023-08-26 18:18:27 -0500

    update
    d

--- src/test/java/Homework18.java
+++ src/test/java/Homework18.java
@@ -2,7 +2,7 @@ import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 public class Homework18 extends BaseTest {
-    @Test(description = "Homework 17 -  addSongToPlaylist")
+    @Test(description = "Homework 18 - playSong")
 
     public void playSong() {
 
