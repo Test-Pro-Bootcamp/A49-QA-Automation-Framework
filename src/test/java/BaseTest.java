5b89776d52a001bdefa6ede25549ba217ac3f9f4
AlinaNikolaienko <alina.salnik@gmail.com>
2023-08-28 13:49:59 -0400

    Homework20
    Homework20 with waits

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,43 +1,40 @@
+import com.github.hemanthsridhar.CSVUtils;
+import com.github.hemanthsridhar.lib.ExtUtils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
-import com.github.hemanthsridhar.CSVUtils;
-import com.github.hemanthsridhar.lib.ExtUtils;
 
-import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
     public WebDriver driver = null;
 
     //public String url = "https://qa.koel.app/";
     public String url;
+
+    WebDriverWait wait;
+
     @BeforeSuite
     static void setupClass() {
-
         WebDriverManager.chromedriver().setup();
-
-
     }
 
     @BeforeMethod
     @Parameters ("BaseURL")
-    public void launchBrowser(String baseUrl) throws InterruptedException {
+    public void launchBrowser(String baseUrl) {
         //      Added ChromeOptions argument below to fix websocket error
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
 
         url = baseUrl;
         driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         //driver.manage().window().maximize();
-
-
-
-
     }
 
     @AfterMethod
@@ -56,17 +53,44 @@ public class BaseTest {
     }
 
     public void providePassword(String password) {
-        WebElement passwordField = driver.findElement(By.cssSelector("[type = 'password']"));
+        WebElement passwordField = driver.findElement(By.cssSelector( "[type = 'password']"));
         passwordField.clear();
         passwordField.sendKeys(password);
-        //passwordField.sendKeys("te$t$tudent");
     }
 
     public void clickSubmitButton() {
         WebElement submitButton = driver.findElement(By.cssSelector("[type = 'submit']"));
         submitButton.click();
-
     }
+
+/*
+    public void provideEmail(String email) {
+        String emailFieldLocator = "[type = 'email']";
+        WebElement emailField = driver.findElement(By.cssSelector(emailFieldLocator));
+        findElementByLocator(By.cssSelector(emailFieldLocator));
+        emailField.clear();
+        emailField.sendKeys(email);
+    }
+
+    public void providePassword(String password) {
+        String passwordFieldLocator = "[type = 'password']";
+        WebElement passwordField = driver.findElement(By.cssSelector(passwordFieldLocator));
+        findElementByLocator(By.cssSelector(passwordFieldLocator));
+        passwordField.clear();
+        passwordField.sendKeys(password);
+    }
+
+    public void clickSubmitButton() {
+        String submitButtonLocator = "[type = 'submit']";
+        WebElement submitButton = driver.findElement(By.cssSelector(submitButtonLocator));
+        findElementByLocator(By.cssSelector(submitButtonLocator));
+        submitButton.click();
+    }
+
+    void findElementByLocator(By locator){
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+    */
 
     @DataProvider(name = "csvData")
     public Object[][] csvDataRead() throws Exception {
