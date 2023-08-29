9ee5b4c8c401ffd725a8e173630d01f87a3dfc63
NataliaGorbunova1 <142173024+NataliaGorbunova1@users.noreply.github.com>
2023-08-29 11:07:29 -0400

    Test passed

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,25 +1,30 @@
+import com.github.hemanthsridhar.CSVUtils;
+import com.github.hemanthsridhar.lib.ExtUtils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.Parameters;
+import org.testng.annotations.*;
 
 import java.time.Duration;
 import java.util.UUID;
 
 public class BaseTest {
-       public WebDriver driver = null;
-       public String url = "https://qa.koel.app/";
-        @BeforeSuite
-        static void setupClass() {
-            WebDriverManager.chromedriver().setup();
-        }
+    public WebDriver driver = null;
+    //    public String url = "https://qa.koel.app/";
+    public String url;
+    WebDriverWait wait;
+
+    @BeforeSuite
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
     @BeforeMethod
     @Parameters({"BaseURL"})
     public void launchBrowser(String baseURL) {
@@ -30,61 +35,46 @@ public class BaseTest {
         options.addArguments("--start-maximized");
         url=baseURL;
         driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-    }
-
-        @AfterMethod
-        public void closeBrowser() {
-            driver.quit();
-    }
-
-        public void navigateToPage() {
-            driver.get(url);
-    }
-        public void provideEmail(String email) {
-            WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
-            emailField.clear();
-            emailField.sendKeys(email);
-    }
-
-        public void providePassword(String password) {
-             WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
-             passwordField.clear();
-             passwordField.sendKeys(password);
-    }
-
-        public void clickSubmit() {
-            WebElement submit = driver.findElement(By.cssSelector("button[type='submit']"));
-            submit.click();
-    }
-
-        public void clickAvatarIcon() {
-             WebElement avatarIcon = driver.findElement(By.cssSelector("img.avatar"));
-             avatarIcon.click();
-    }
-
-        public void isAvatarDisplayed() {
-            WebElement avatarIcon = driver.findElement(By.cssSelector("img[class='avatar']"));
-            Assert.assertTrue(avatarIcon.isDisplayed());
-    }
-
-        public String generateRandomName() {
-            return UUID.randomUUID().toString().replace("-", "");
-    }
-
-
-        public void loginWithCorrectCreds() {
-            navigateToPage();
-            provideEmail("natalia.gorbunova@testpro.io");
-            providePassword("tester2023");
-            clickSubmit();
+        //instantiate Explicit wait
+        wait = new WebDriverWait(driver,Duration.ofSeconds(20));
 
     }
-        public void enterText(By inputLocator, String inputText) {
-            WebElement searchInput = driver.findElement(inputLocator);
-            searchInput.click();
-            searchInput.clear();
-            searchInput.sendKeys(inputText);
+
+    @AfterMethod
+    public void closeBrowser() {
+        driver.quit();
     }
 
+    public void navigateToPage() {
+        driver.get(url);
+    }
+
+    public void provideEmail(String email) {
+        enterText(By.cssSelector("input[type='email']"),email);
+    }
+
+    public void providePassword(String password) {
+        enterText(By.cssSelector("input[type='password']"),password);
+    }
+
+    public void clickSubmit() {
+        clickOnElement(By.cssSelector("button[type='submit']"));
+    }
+
+    protected void loginCorrectCred() {
+        navigateToPage();
+        provideEmail("demo@class.com");
+        providePassword("te$t$tudent");
+    }
+
+    void clickOnElement(By locator){
+        WebElement el= wait.until(ExpectedConditions.elementToBeClickable(locator));
+        el.click();
+    }
+    void enterText(By locator, String text){
+        WebElement el= wait.until(ExpectedConditions.elementToBeClickable(locator));
+        el.click();
+        el.clear();
+        el.sendKeys(text);
+    }
 }
