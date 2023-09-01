fe64f0b5d9ec905c2df6b9e596ea56e189894d55
NataliaGorbunova1 <142173024+NataliaGorbunova1@users.noreply.github.com>
2023-09-01 12:22:55 -0400

    Test passed

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -16,7 +17,7 @@ import java.util.UUID;
 
 public class BaseTest {
     public WebDriver driver = null;
-    //    public String url = "https://qa.koel.app/";
+    public static Actions actions = null;
     public String url;
     WebDriverWait wait;
 
@@ -25,10 +26,9 @@ public class BaseTest {
         WebDriverManager.chromedriver().setup();
     }
 
-    @BeforeMethod
+    @BeforeClass
     @Parameters({"BaseURL"})
     public void launchBrowser(String baseURL) {
-        //Added ChromeOptions argument below to fix websocket error
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         options.addArguments("--disable-notifications");
@@ -37,10 +37,11 @@ public class BaseTest {
         driver = new ChromeDriver(options);
         //instantiate Explicit wait
         wait = new WebDriverWait(driver,Duration.ofSeconds(20));
+        actions = new Actions(driver);
 
     }
 
-    @AfterMethod
+    @AfterClass
     public void closeBrowser() {
         driver.quit();
     }
@@ -58,23 +59,30 @@ public class BaseTest {
     }
 
     public void clickSubmit() {
-        clickOnElement(By.cssSelector("button[type='submit']"));
+        clickOnElement(By.cssSelector("[type='submit']"));
     }
 
-    protected void loginCorrectCred() {
+     public void loginCorrectCred() {
         navigateToPage();
         provideEmail("demo@class.com");
         providePassword("te$t$tudent");
+        clickSubmit();
     }
 
-    void clickOnElement(By locator){
+    public void clickOnElement(By locator){
         WebElement el= wait.until(ExpectedConditions.elementToBeClickable(locator));
         el.click();
     }
-    void enterText(By locator, String text){
+    public void enterText(By locator, String text){
         WebElement el= wait.until(ExpectedConditions.elementToBeClickable(locator));
         el.click();
         el.clear();
         el.sendKeys(text);
     }
+    public void checkShowSuccess() {
+        WebElement notification = wait.until(ExpectedConditions.
+                visibilityOfElementLocated(By.cssSelector("div.success.show")));
+        Assert.assertTrue(notification.isDisplayed());
+    }
+
 }
