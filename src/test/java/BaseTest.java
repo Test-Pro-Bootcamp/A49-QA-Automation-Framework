eabca8d92d18c3a85acac2740276cf98cada4f43
NataliaGorbunova1 <142173024+NataliaGorbunova1@users.noreply.github.com>
2023-09-01 15:09:19 -0400

    Test passed

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,5 +1,3 @@
-import com.github.hemanthsridhar.CSVUtils;
-import com.github.hemanthsridhar.lib.ExtUtils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -9,80 +7,60 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 import org.testng.annotations.*;
 
 import java.time.Duration;
-import java.util.UUID;
 
 public class BaseTest {
-    public WebDriver driver = null;
+
+    public static WebDriver driver = null;
+    public static String url = null;
+    public static WebDriverWait wait = null;
     public static Actions actions = null;
-    public String url;
-    WebDriverWait wait;
 
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
     }
 
-    @BeforeClass
+    @BeforeMethod
     @Parameters({"BaseURL"})
-    public void launchBrowser(String baseURL) {
+    public void launchBrowser(String BaseURL) {
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-        options.addArguments("--disable-notifications");
-        options.addArguments("--start-maximized");
-        url=baseURL;
+        options.
+                addArguments("--disable-notifications", "--remote-allow-origins=*", "--incognito", "--start-maximized");
+        options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
         driver = new ChromeDriver(options);
-        //instantiate Explicit wait
-        wait = new WebDriverWait(driver,Duration.ofSeconds(20));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         actions = new Actions(driver);
-
+        url = BaseURL;
+        driver.get(url);
     }
 
-    @AfterClass
+    @AfterMethod(alwaysRun = true)
     public void closeBrowser() {
         driver.quit();
     }
 
-    public void navigateToPage() {
-        driver.get(url);
-    }
-
     public void provideEmail(String email) {
-        enterText(By.cssSelector("input[type='email']"),email);
+        WebElement emailField = wait.until(ExpectedConditions.
+                visibilityOfElementLocated(By.cssSelector("input[type='email']")));
+        emailField.clear();
+        emailField.sendKeys(email);
     }
 
     public void providePassword(String password) {
-        enterText(By.cssSelector("input[type='password']"),password);
+        WebElement passwordField = wait.until(ExpectedConditions.
+                visibilityOfElementLocated(By.cssSelector("input[type='password']")));
+        passwordField.clear();
+        passwordField.sendKeys(password);
     }
 
     public void clickSubmit() {
-        clickOnElement(By.cssSelector("[type='submit']"));
+        WebElement submit = wait.until(ExpectedConditions.
+                visibilityOfElementLocated(By.cssSelector("button[type='submit']")));
+        submit.click();
     }
-
-     public void loginCorrectCred() {
-        navigateToPage();
-        provideEmail("demo@class.com");
-        providePassword("te$t$tudent");
-        clickSubmit();
-    }
-
-    public void clickOnElement(By locator){
-        WebElement el= wait.until(ExpectedConditions.elementToBeClickable(locator));
-        el.click();
-    }
-    public void enterText(By locator, String text){
-        WebElement el= wait.until(ExpectedConditions.elementToBeClickable(locator));
-        el.click();
-        el.clear();
-        el.sendKeys(text);
-    }
-    public void checkShowSuccess() {
-        WebElement notification = wait.until(ExpectedConditions.
-                visibilityOfElementLocated(By.cssSelector("div.success.show")));
-        Assert.assertTrue(notification.isDisplayed());
-    }
-
 }
