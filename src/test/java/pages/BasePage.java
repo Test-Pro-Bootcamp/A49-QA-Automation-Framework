38c4bbddeb31f602cc984a7a3d6f9d4a10bfc821
suemoua <sue.moua@testpro.io>
2023-09-07 18:08:41 -0500

    Update
    clean up spaces so the codes will look cleaner

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,10 +1,7 @@
 package pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.BeforeMethod;
 
-import java.time.Duration;
 
 public class BasePage {
     WebDriver driver;
--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.BeforeMethod;
+
 
 import java.time.Duration;
 public class LoginPage extends BasePage {
