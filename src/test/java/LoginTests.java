1e20f0b74f8ea2f8f610c2cb816a5707a2beefe6
LiudmilaChernyh <liudmila.chernyh@testpro.io>
2023-09-08 13:42:37 -0700

    Create LoginTests.java

---
+++ src/test/java/LoginTests.java
@@ -0,0 +1,21 @@
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+
+public class LoginTests extends BaseTest{
+    LoginPage loginPage;
+    @BeforeMethod
+    void setup(){
+        loginPage = new LoginPage(driver);
+        driver.get(url);
+    }
+    @Test
+    public void validLogin(){
+        loginPage.loginCorrectCred();
+    }
+    @Test
+    public void validLoginOnceLoggedIn(){
+        loginPage.loginCorrectCred();
+    }
+}
