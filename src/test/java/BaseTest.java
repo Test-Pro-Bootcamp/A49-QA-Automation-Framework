d9ef958e4537673f31be6bb5d44dbd738c01b69f
LiudmilaChernyh <liudmila.chernyh@testpro.io>
2023-09-08 13:42:28 -0700

    homework-24
    first try

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,11 +1,62 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.BeforeSuite;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.testng.annotations.*;
+import pages.BasePage;
+
+import java.net.MalformedURLException;
+import java.net.URI;
 
 public class BaseTest {
+    public WebDriver driver ;
+    public String url = "https://qa.koel.app/";
 
-    @BeforeSuite
-    static void setupClass() {
+
+    @BeforeClass
+    public void setupClass() throws MalformedURLException {
+        String browser = System.getProperty("browser");
+        driver=setupBrowser(browser);
+    }
+    @AfterClass
+    public void closeBrowser() {
+        driver.quit();
+    }
+    WebDriver setupBrowser(String browser) throws MalformedURLException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+
+        String gridURL = "http://192.168.1.105:4444";
+        switch(browser) {
+            case "chrome":
+                return setupChrome();
+            case "safari":
+                return setupSafari();
+            case "grid-chrome":
+                caps.setCapability("browserName", "chrome");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
+
+            default:
+                return setupChrome();
+        }
+    }
+
+    public WebDriver setupSafari(){
+        WebDriverManager.safaridriver().setup();
+        driver =new SafariDriver();
+        return driver;
+    }
+    public WebDriver setupChrome(){
         WebDriverManager.chromedriver().setup();
+        //Added ChromeOptions argument below to fix websocket error
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--disable-notifications");
+        options.addArguments("--start-maximized");
+        driver = new ChromeDriver(options);
+        return driver;
     }
 }
