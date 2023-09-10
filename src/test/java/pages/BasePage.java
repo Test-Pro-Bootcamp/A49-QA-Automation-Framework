b4e440d5411c13c3d756f952ab5ee75a258b69b1
suemoua <sue.moua@testpro.io>
2023-09-10 14:08:27 -0500

    update

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -2,7 +2,6 @@ package pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
-
 public class BasePage {
     WebDriver driver;
     public BasePage(WebDriver driver){
