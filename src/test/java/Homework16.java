7bc2cbdfc3b984567257021705ec8a64cce6914c
MavisAffum <133819225+MavisAffum@users.noreply.github.com>
2023-09-10 21:02:42 -0500

    registration navigation
    Completed navigation test, added a method

--- src/test/java/Homework16.java
+++ src/test/java/Homework16.java
@@ -1,28 +1,36 @@
 import org.openqa.selenium.By;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 public class Homework16 extends BaseTest {
     @Test
-    public void registrationNavigation(){
+    public  void registrationNavigation() {
 //Adding chromeOptions arguments below to fix websocket
 
         chromeOptions options = new chromeOptions();
         options.addArguments("--remote-allow-origins");
-        //GIVEN: url is set and opened with valid credentials
-        webdriver driver= new webdriver(options);
+        options.addArguments("---starts maximize");
+
+
+        webDriver driver = new webDriver(options);
         driver.manager().timeout().implicitelyWait(duration.ofseconds(10));
     }
+
     //WHEN: user opens url
     String url = "https://qa.koel.app/";
     driver.get(url);
+
     //AND: user searches element
     webElement registrationLink = driver.findElement(By.cssSelector("[href = 'registration']"));
     registrationLink.click;
 
 //THEN: Koel app should redirect
-    String.registrationurl = "https://qa.koel.app/registration";
-    Assert.assertEquals(driver.getcurrenturl(), registration);
- driver.quit();
+    String.registrationurl ="https://qa.koel.app/registration";
+    Assert.assertEquals(driver.getcurrenturl(),registration);
 
+    @AfterMethod
+            driver.quit();
 
+}
+}
