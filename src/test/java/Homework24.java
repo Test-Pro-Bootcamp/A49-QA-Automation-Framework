cbecb8e181806831e5e28cd34acd6359ff5e0477
Svitlana Shkribliak <svitlana.shkribliak@testpro.io>
2023-09-12 11:28:59 -0500

    Homework24
    Added Assert

--- src/test/java/Homework24.java
+++ src/test/java/Homework24.java
@@ -1,18 +1,23 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.BasePage;
+import pages.HomePage;
 import pages.LoginPage;
 import pages.PlaylistPage;
 
 public class Homework24 extends BaseTest {
-    String playlistName;
     LoginPage loginPage;
-    BasePage basePage;
-    PlaylistPage playlistPage;
+    HomePage homePage;
 
     @Test
     public void login() {
         loginPage = new LoginPage(driver);
+        homePage = new HomePage(driver);
         loginPage.loginWithCorrectCred();
+        homePage.avatarImgIsDisplayed();
     }
 
 }
