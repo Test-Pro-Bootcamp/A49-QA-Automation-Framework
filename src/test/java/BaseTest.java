b7f943b56c4461bfb4018f590442c41b92b1c331
suemoua <sue.moua@testpro.io>
2023-09-11 23:26:47 -0500

    update hw-25

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -6,60 +6,46 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.*;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
 import java.util.HashMap;
+import java.time.Duration;
 
 
 public class BaseTest {
-    public WebDriver driver ;
-    public String url = "https://qa.koel.app/";
-    @BeforeSuite
-    public void setupSuit() throws MalformedURLException {
-        String browser = System.getProperty("browser");
-        driver=setupBrowser(browser);
+    private static final ThreadLocal<WebDriver> THREAD_LOCAL = new ThreadLocal<>();
+    public static WebDriver driver ;
+    public static WebDriver getThreadLocal() {
+        return THREAD_LOCAL.get();
     }
-    @AfterClass
-    public void closeBrowser() {
-        driver.quit();
+
+    public static String url = "https://qa.koel.app/";
+    @BeforeMethod
+    @Parameters({"baseURL"})
+    public void setUpBrowser(@Optional String baseURL) throws MalformedURLException {
+        THREAD_LOCAL.set(pickBrowser(System.getProperty("browser")));
+        THREAD_LOCAL.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        THREAD_LOCAL.get().manage().window().maximize();
+        THREAD_LOCAL.get().manage().deleteAllCookies();
+        System.out.println(
+                "Browser setup by Thread " + Thread.currentThread().getId() + " and Driver reference is : " + getThreadLocal());
     }
-    WebDriver setupBrowser(String browser) throws MalformedURLException {
-        DesiredCapabilities caps = new DesiredCapabilities();
-        String gridURL = "http://192.168.1.105:4444";
-        switch (browser) {
-            case "chrome" -> {
-                return setupChrome();
-            }
-            case "edge" -> {
-                return setupEdge();
-            }
-            case "cloud" -> {
-                return lambdaTest();
-            }
-            case "grid-chrome" -> {
-                caps.setCapability("browserName", "chrome");
-                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
-            }
-            case "grid-edge" -> {
-                caps.setCapability("browserName", "edge");
-                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
-            }
-            default -> {
-                return setupChrome();
-            }
-        }
-    }
-    public WebDriver setupChrome(){
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-        options.addArguments("--disable-notifications");
-        options.addArguments("--start-maximized");
-        driver = new ChromeDriver(options);
-        return driver;
+    public WebDriver lambdaTest ()throws MalformedURLException{
+        String hubURL = "https://hub.lambdatest.com/wd/hub";
+
+        ChromeOptions browserOptions = new ChromeOptions();
+        browserOptions.setPlatformName("Windows 10");
+        browserOptions.setBrowserVersion("117.0");
+        HashMap<String, Object> ltOptions = new HashMap<>();
+        ltOptions.put("username", "riddik.ozzie");
+        ltOptions.put("accessKey", "UAy2ARLWKyzJIG9ue8B12dRA151OKgnAsLmlqm5HW5GUkmqHuO");
+        ltOptions.put("project", "Untitled");
+        ltOptions.put("selenium_version", "4.0.0");
+        ltOptions.put("w3c", true);
+        browserOptions.setCapability("LT:Options", ltOptions);
+        return new RemoteWebDriver(new URL(hubURL), browserOptions);
     }
     public WebDriver setupEdge(){
         WebDriverManager.edgedriver().setup();
@@ -70,20 +56,32 @@ public class BaseTest {
         driver =new EdgeDriver(options);
         return driver;
     }
-    public WebDriver lambdaTest ()throws MalformedURLException{
-        String hubURL = "https://hub.lambdatest.com/wd/hub";
+    public WebDriver pickBrowser(String browser) throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        String gridURL = "http://192.168.1.105:4444";
+        switch (browser) {
+            case "edge":
+                return setupEdge();
+            case "cloud":
+                return lambdaTest();
+            case "grid-chrome":
+                capabilities.setCapability("browserName", "chrome");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), capabilities);
+            case "grid-edge":
+                capabilities.setCapability("browserName", "edge");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), capabilities);
 
-        ChromeOptions browserOptions = new ChromeOptions();
-        browserOptions.setPlatformName("Windows 10");
-        browserOptions.setBrowserVersion("117.0");
-        HashMap<String, Object> ltOptions = new HashMap<String, Object>();
-        ltOptions.put("username", "riddik.ozzie");
-        ltOptions.put("accessKey", "UAy2ARLWKyzJIG9ue8B12dRA151OKgnAsLmlqm5HW5GUkmqHuO");
-        ltOptions.put("project", "Untitled");
-        ltOptions.put("selenium_version", "4.0.0");
-        ltOptions.put("w3c", true);
-        browserOptions.setCapability("LT:Options", ltOptions);
-
-        return new RemoteWebDriver(new URL(hubURL), browserOptions);
+            default:
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions optionsChrome = new ChromeOptions();
+                optionsChrome.addArguments("--disable-notifications","--remote-allow-origins=*", "--incognito","--start-maximized");
+                optionsChrome.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
+                return driver = new ChromeDriver(optionsChrome);
+        }
+    }
+    @AfterMethod
+    public void tearDown() {
+        THREAD_LOCAL.get().close();
+        THREAD_LOCAL.remove();
     }
 }
--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,60 +1,39 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.LoginPage;
 public class LoginTests extends BaseTest {
-    LoginPage loginPage;
+    static LoginPage loginPage;
     @BeforeMethod
     void setup() {
-        loginPage = new LoginPage(driver);
-        driver.get(url);
+        loginPage = new LoginPage(getThreadLocal());
+        getThreadLocal().get(url);
     }
     @Test
-    public void validLogin() {
-        WebElement loginInput = driver.findElement(By.cssSelector("[type='email']"));
-        loginInput.click();
-        loginInput.clear();
-        loginInput.sendKeys("xu_moua@hotmail.com");
-//      AND I Enter valid password
-        WebElement passwordInput = driver.findElement(By.cssSelector("[type='password']"));
-        passwordInput.click();
-        passwordInput.clear();
-        passwordInput.sendKeys("TestStudent!1");
-//      AND I click Log in
-        WebElement loginBtn = driver.findElement(By.cssSelector("[type='submit']"));
-        loginBtn.click();
-//      THEN Avatar is Displayed
-        WebElement avatar = driver.findElement(By.cssSelector(".avatar"));
-        Assert.assertTrue(avatar.isDisplayed());
-//        loginPage.loginCorrectCred();
-//        WebElement avatar = driver.findElement(By.cssSelector("[class='music']"));
-//        Assert.assertTrue(avatar.isDisplayed());
-        Assert.assertEquals(driver.getCurrentUrl(), "www.qa.qoel.app/#!/");
+    public static void validLogin() {
+        LoginPage loginPage = new LoginPage(getThreadLocal());
+        loginPage.loginCorrectCred();
+        Assert.assertEquals(driver.getCurrentUrl(), url);
     }
     @Test
-    public void invalidPasswordEmail() {
+    public static void invalidPasswordEmail() {
         loginPage.provideEmail("incorrectEmail")
                 .providePassword("incotrerctPwd")
                 .clickSubmit();
         Assert.assertEquals(driver.getCurrentUrl(), url);
     }
     @Test
-    public void emptyEmail() {
+    public static void emptyEmail() {
         loginPage.provideEmail("")
                 .providePassword("incotrerctPwd")
                 .clickSubmit();
         Assert.assertEquals(driver.getCurrentUrl(), url);
     }
     @Test
-    public void emptyEmailPassword() {
+    public static void emptyEmailPassword() {
         loginPage.provideEmail("")
                 .providePassword("")
                 .clickSubmit();
         Assert.assertEquals(driver.getCurrentUrl(), url);
-
-
-
     }
 }
