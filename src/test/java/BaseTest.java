81f528022b79b5f058e0690f91e41ce8240d7a4a
aparajita135 <142899090+aparajita135@users.noreply.github.com>
2023-09-18 12:08:15 -0400

    homework18

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,11 +1,74 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Parameters;
+
+import java.time.Duration;
 
 public class BaseTest {
+
+    public WebDriver driver= null;
+
+    public String url;
+
 
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
     }
-}
+
+    @BeforeMethod
+    @Parameters({"BaseURL"})
+        public void LaunchBrowser(String BaseURL){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+  @AfterMethod
+
+     public void navigateToPage(){
+        String url ="https://qa.koel.app";
+        driver.get(url);
+ }
+
+    public void ProvideEmail(String email) {
+        WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
+        emailField.click();
+        emailField.sendKeys("Demo@testpro.io");
+
+    }
+    public void ProvidePassword(String password){
+        WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
+        passwordField.clear();
+        passwordField.sendKeys("te$t$tudent");
+
+    }
+
+  public void clickSubmit() throws InterruptedException{
+        WebElement submit = driver.findElement(By.cssSelector("Button[type='submit']"));
+        submit.click();
+      Thread.sleep(3000);
+  }
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
