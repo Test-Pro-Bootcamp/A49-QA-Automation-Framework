3a291e20aef8ffa91badbefa5ecbbbe85e1fc7e5
emilianocastillo <emiliano.castillo@testpro.io>
2023-09-19 14:31:44 -0400

    Homework 27 refactor

--- src/test/java/stepDefinitions/LoginSteps.java
+++ src/test/java/stepDefinitions/LoginSteps.java
@@ -1,53 +1,38 @@
 package stepDefinitions;
 import io.cucumber.java.After;
-import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
-import java.time.Duration;
+import org.testng.annotations.BeforeMethod;
+import pageObjects.LoginPage;
 public class LoginSteps {
-    WebDriver driver;
-    WebDriverWait wait;
-    @Before
-    public void iOpenBrowser() {
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-notifications");
-        options.addArguments("--remote-allow-origins=*");
-        driver = new ChromeDriver(options);
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    @BeforeMethod
+    public static void iOpenBrowser() {
+        LoginPage.iOpenLogin();
     }
     @After
-    public void closeBrowser(){
-        driver.quit();
+    public static void closeBrowser(){
+        LoginPage.closeBrowser();
     }
     @Given("I open Login")
     public void iOpenLogin() {
-        driver.get("https://qa.koel.app/");
+        LoginPage.iOpenLogin();
     }
     @When("I enter email {string}")
     public void iEnterEmail(String email) {
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[type='email']"))).sendKeys(email);
+        LoginPage.iEnterEmail(email);
     }
     @And("I enter password {string}")
     public void iEnterPassword(String password) {
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[type='password']"))).sendKeys(password);
+        LoginPage.iEnterPassword(password);
     }
     @And("I submit")
     public void iSubmit() {
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[type='submit']"))).click();
+        LoginPage.iSubmit();
     }
     @Then("I aam logged in")
     public void iAamLoggedIn() {
-        Assert.assertTrue(wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("img.avatar"))).isDisplayed());
+        LoginPage.iAamLoggedIn();
     }
 }
