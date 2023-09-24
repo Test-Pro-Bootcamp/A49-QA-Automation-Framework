c0fb8b011a6bba114ff28947762f824631e97dc0
StetsonCapital <amx2044@gmail.com>
2023-09-24 10:44:33 -0500

    Create BasePage.java

---
+++ src/test/java/BasePage.java
@@ -0,0 +1,24 @@
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import java.time.Duration;
+import java.util.UUID;
+
+public class BasePage {
+    WebDriver driver;
+
+    WebDriverWait wait;
+
+    public BasePage(WebDriver driver){
+        this.driver = driver;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        PageFactory.initElements(driver,this);
+    }
+}
