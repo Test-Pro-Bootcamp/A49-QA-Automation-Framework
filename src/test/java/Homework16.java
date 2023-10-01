c686ffb94dfef29b5674022e431cc0a9d4669202
KCashwell <144808858+KCashwell@users.noreply.github.com>
2023-10-01 17:24:31 -0400

    Create Homework16.java
    Homework

---
+++ src/test/java/Homework16.java
@@ -0,0 +1,22 @@
+
+public class Homework16 extends BaseTest {
+    @Test
+    public void registrationNavigation() {
+//      Added ChromeOptions argument below to fix websocket error
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        String url = "https://qa.koel.app/";
+        driver.get(url);
+
+        WebElement registrationlink = driver.findElement(By.cssSelector("[href='registration']" +));
+        registrationlink.click();
+
+        String registrationUrl = "https://qa.koel.app/registration";
+        Assert.assertEquals(driver.getCurrentURl(), registrationUrl);
+        driver.quit();
+    }}
+}
