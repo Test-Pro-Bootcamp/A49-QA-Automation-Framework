2bb2c8b7f89aa15df3a51b749c1547b4cd2bd49d
rijoal <114624987+rijoal@users.noreply.github.com>
2023-10-24 20:48:53 -0700

    Homework29
    Trying to run tests in parallel having issues

--- src/test/java/stepDefinition/AddSongToNewList.java
+++ src/test/java/stepDefinition/AddSongToNewList.java
@@ -10,7 +10,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import pages.LoginPage;
 import pages.SongsPage;
 
