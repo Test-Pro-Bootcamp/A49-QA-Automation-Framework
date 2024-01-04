ff10c3568cf574d49fb01af05c05543ff20662e3
dawit-gebeyehu <dawit.gebeyehu@testpro.io>
2024-01-03 22:17:07 -0600

    Update Homework15.java

--- src/test/java/Homework15.java
+++ src/test/java/Homework15.java
@@ -14,7 +14,6 @@ public class Homework15 extends BaseTest {
 //      Added ChromeOptions argument below to fix websocket error
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
-
         WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
